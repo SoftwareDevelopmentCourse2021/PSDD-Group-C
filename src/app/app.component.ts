08113ebd6972ff41af40165fac2b8b2ffa219c76
Nathan Turvey <61296953+Clutch-T@users.noreply.github.com>
2021-08-22 16:29:01 +1000

    Test-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,10 @@
-import {Component, ViewChild} from '@angular/core';
-import {MatSidenav} from '@angular/material/sidenav';
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    // ViewChild is a method that allows you 
-    // to get a html element from your html page
- @ViewChild('snav') snav: MatSidenav;
-
-    close() {
-        this.snav.close();
-    }
+  title = 'licenceLogger';
 }
