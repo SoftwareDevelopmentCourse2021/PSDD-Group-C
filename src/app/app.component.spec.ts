f9d9effe6b3c5d53478b8d5800c9519360c9df53
Nathan Turvey <61296953+Clutch-T@users.noreply.github.com>
2021-08-22 17:12:01 +1000

    test-2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'licenceLogger'`, () => {
+  it(`should have as title 'fuck-me-in-the-ass-angular-is-stupid'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('licenceLogger');
+    expect(app.title).toEqual('fuck-me-in-the-ass-angular-is-stupid');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('licenceLogger app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('fuck-me-in-the-ass-angular-is-stupid app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'licenceLogger';
+  title = 'fuck-me-in-the-ass-angular-is-stupid';
 }
